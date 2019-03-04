3e8d4a098e5c0e552935e0860eede21c425e2ccf
Vincent BONNIER <vincent@kollebolle.com>
2019-03-04 16:49:07 +0100

    initial commit

---
+++ js/mfrbquizz.js
@@ -0,0 +1,99 @@
+var myFullpage = new fullpage('#fullpage', {
+    sectionsColor: [
+      '#fee70e', 
+      '#fee70e', 
+      '#fee70e', 
+      '#fee70e', 
+      '#fee70e',
+      '#fee70e',
+      '#fee70e',
+      '#fee70e',
+      '#fee70e',
+      '#fee70e',
+      '#fee70e'
+      ],
+    anchors: [
+      'intro', 
+      'q1', 
+      'q2', 
+      'q3', 
+      'q4', 
+      'q5', 
+      'q6', 
+      'surf',
+      'hamster',
+      'robot',
+      'philo',
+    ],
+    menu: '#menu',
+    continuousVertical: true,
+    afterLoad: function(anchorLink, index){
+      console.log("AFTER LOAD - anchorLink:" +anchorLink + " index:" +index );
+    },
+    onLeave: function(index, nextIndex, direction){
+      console.log("ONLEAVE - index:" +index + " nextIndex:" +nextIndex  + " direction:" + direction);
+
+    },
+  });
+
+  $( document ).ready(function() {
+
+    var score = {
+      hamster: 0, 
+      philo: 0, 
+      robot: 0,
+      surf:0
+    };
+    
+    function updateCounter() {
+      $("#hamsterCounterValue").html(score.hamster);
+      $("#philoCounterValue").html(score.philo);
+      $("#robotCounterValue").html(score.robot);
+      $("#surfCounterValue").html(score.surf);
+    }
+
+    //on click next step ! 
+    $( ".btn" ).click(function(e) {
+      //prevent default
+      e.preventDefault();
+      
+      //update counter
+      if($(this).hasClass( "hamster" )) {
+        score.hamster++;
+        updateCounter();
+      } else if ($(this).hasClass( "philo" )) {
+        score.philo++;
+        updateCounter();
+      } else if ($(this).hasClass( "surf" )) {
+        score.surf++;
+        updateCounter();
+      } else if ($(this).hasClass( "robot" )) {
+        score.robot++;
+          
+      }
+      //update counter
+      updateCounter();
+
+      //Go to next slide
+      if(fullpage_api.getActiveSection().anchor == "q6") {
+        //pick max counter (see https://stackoverflow.com/questions/27376295/getting-key-with-the-highest-value-from-object )
+        var result = Object.keys(score).reduce(function(a, b){ return score[a] > score[b] ? a : b });
+
+        //goto correspondig answer
+        if(result == "hamster") {
+          fullpage_api.moveTo('hamster', 1);
+        } else if(result == "robot") {
+          fullpage_api.moveTo('robot', 1);
+        } else if(result == "philo") {
+          fullpage_api.moveTo('philo', 1);
+        } else if(result == "surfer") {
+          fullpage_api.moveTo('surfer', 1);
+        }
+        
+      } else {
+        //move to next session
+        fullpage_api.moveSectionDown();
+      }
+
+    });
+  });
